a44cf47ac1fe6eb22afb553c0222afeffa03dbd8
akhileshsonwani <a.sonwani1@gmail.com>
2023-11-01 20:10:05 +0530

    project

---
+++ Main.java
@@ -0,0 +1,21 @@
+class Employee
+{
+    String name;
+    int id;
+    Address add;
+}
+class Address
+{
+    String country;
+    String state;
+
+}
+class Main
+{
+    public static void main(String[]args)
+    {
+        Employee emp=new Employee();
+        emp.name=
+
+    }
+}
